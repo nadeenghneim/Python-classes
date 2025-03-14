445f8d0a9f9b4c0b9c0736c1ecffef9c61bc9078
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-03-14 08:44:04 +0200

    act2

---
+++ day 3/act2.py
@@ -0,0 +1,17 @@
+name='cupcake'
+age=9
+numb=17.1
+yes= True
+
+print('datatype of name is:', type(name))
+print('datatype of age is:', type(age))
+print('datatype of numb is:', type(numb))
+print('datatype of yes is:', type(yes))
+
+print("\n",'After conversions:')
+age=float(age)
+print(type(age),age)
+numb=int(numb)
+print(type(numb),numb)
+yes=str(yes)
+print(type(yes),yes)
