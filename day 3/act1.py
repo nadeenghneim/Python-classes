cbde1251a13fd1faa540f4ae2d5942852da67445
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-03-14 08:22:40 +0200

    activity 1 day 3

---
+++ day 3/act1.py
@@ -0,0 +1,8 @@
+a= 'cupcake'
+b=9
+c=17.5
+d= False
+print('The datatype of a is:',type(a))
+print('The datatype of b is:',type(b))
+print('The datatype of c is:',type(c))
+print('The datatype of d is:',type(d))
