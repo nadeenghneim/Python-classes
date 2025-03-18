bdc5139e3f5b16b2cf7681c2efa6f0c1fc1ff1b1
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-03-18 15:23:48 +0200

    act3

---
+++ day 4/act3.py
@@ -0,0 +1,11 @@
+math=int(input('enter your math grade:' ))
+biology=int(input('enter your biology grade:' ))
+chemistry=int(input('enter your chemistry grade:' ))
+physics=int(input('enter your physics grade:' ))
+english=int(input('enter your english grade:' ))
+
+total= math+biology+chemistry+physics+english
+
+print("your total amount is:",total)
+percentage=(total/500)*100
+print("the percentage is:", percentage)
