af0586585f351837f9ec8f1f3d2867d70ff23e7b
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-03-18 15:00:41 +0200

    act1

---
+++ day 4/act1.py
@@ -0,0 +1,11 @@
+print("These are your grades!")
+s1=75
+s2=85
+s3=99
+s4=100
+s5=60
+total= s1+s2+s3+s4+s5
+print('The total of your grades is:',total)
+
+avg= total/5
+print('your average is:', avg)
