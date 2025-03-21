f67e7807a0bb6e8e52670e01abeca2f08eaa03a8
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-03-21 10:38:24 +0200

    act3 and act4

---
+++ day 5/act3.py
@@ -0,0 +1,8 @@
+number=float(input("Enter a number:"))
+if number<1:
+    print("It's a decimal")
+    print("It is inside if block 1.")
+else:
+    print('it is greater than one!')
+    print('it is inside the else block.')
+
---
+++ day 5/act4.py
@@ -0,0 +1,6 @@
+num=int(input("enter your desired integer: "))
+
+if num%2==0:
+    print(f"the number {num} is even.")
+else: 
+    print('the number is odd!')
