63eb0e499ec035c8ea85be6612409842ddb65bb8
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-03-25 16:12:09 +0200

    act2

---
+++ day 6/act2.py
@@ -0,0 +1,15 @@
+x=int(input("enter the first(x) integer of your choice: "))
+y=int(input("enter the second(y) integer of your choice: "))
+z=int(input("enter the third(z) integer of your choice: "))
+
+
+print("is x not equal to y?")
+if x!=y:
+    print("yes")
+else:
+    print("no")
+print("is y equal to one and is z not equal to it")
+if (y==1)!=z:
+    print("yes")
+else:
+    print("no")
