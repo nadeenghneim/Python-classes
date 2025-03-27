cd3881e2638c567f715c7a90940789b26b458ee0
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-03-27 16:00:29 +0200

    act1

---
+++ day 7/act1.py
@@ -0,0 +1,19 @@
+x="hello"
+if (type(x) is str):
+    print("true")
+    
+else:
+    print('false')
+
+y=96
+if (type(y) is not int):
+    print("false")
+else:
+    print("true")
+
+n= 90
+m=98
+if (m is n):
+    print("m and n are the same")
+else:
+    print("m and n are not the same")
