37437c0708fa520952513e3fcf42849e0f2a569f
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-04-01 01:37:21 +0300

    alphabet project

---
+++ day 6/alphabetproj.py
@@ -0,0 +1,16 @@
+character= input("enter any single character: ")
+
+if character>="a" and character<= "z":
+    print("this character is part of the alphabet")
+elif character>="A" and character<= "Z":
+    print("this character is part of the alphabet.")
+else:
+    print("this character is not part of the alphabet.")
+
+
+
+
+
+
+
+
