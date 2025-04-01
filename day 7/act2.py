5ac000c531acbaa622979ba71ae7f5a4cb7fbf87
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-04-01 17:05:51 +0300

    act1

--- day 7/act2.py
+++ day 7/act2.py
@@ -1,7 +1,7 @@
 a=9
 b=7
 print("a>>1=",a>>1)
-print("b>>3=",b>>3)
+print("b>>1=",b>>1)
 
 print("a<<1=",a<<1)
 
---
+++ day 8/act1.py
@@ -0,0 +1,18 @@
+x= 4
+y=3
+z=1
+n=5
+
+equation= (x**y)/n*z
+print(equation)
+
+eq2= (n/y)+y**x
+print(eq2)
+
+age= int(input("enter your age: "))
+username= input("enter your username: ")
+
+if username=="nadeenghneim" and age==17 or username=="nadeen" and age==18:
+    print("welcome to your account")
+else:
+    print("incorrect info, try again!")
