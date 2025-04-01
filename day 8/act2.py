3e6731c7a9b055e2347f88e464b4c9b41ee02401
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-04-01 17:10:38 +0300

    act2

---
+++ day 8/act2.py
@@ -0,0 +1,7 @@
+num=int(input("enter the number you want to divide:"))
+den=int(input("enter the number you want to divide by:"))
+
+if num%den == 0:
+    print("your number is divisible ")
+else:
+    print("it is not divisible")
