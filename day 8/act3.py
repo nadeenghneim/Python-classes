95bc9d3ed349cdca06cba8e6630652a15d404abe
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-04-01 17:25:35 +0300

    act3

---
+++ day 8/act3.py
@@ -0,0 +1,10 @@
+wrmean= 50
+wrnumber=70
+rinumber=45
+amountofnum=30
+sum= amountofnum*wrmean
+valuen= sum-((rinumber)-(wrnumber))
+print("the new value is",valuen)
+
+meanri=valuen/amountofnum
+print("the new and correct mean is ", meanri)
