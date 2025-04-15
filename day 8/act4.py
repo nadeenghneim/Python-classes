be71226b80ad01bff1e274166485ba2a4c7373c3
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-04-15 15:50:30 +0300

    lesson 9 project

--- day 8/act4.py
+++ day 8/act4.py
@@ -1,5 +1,5 @@
-speed1=
-speed2=
-speed3=
-avgspeed=
+speed1=float(input("enter your first speed in miles per hour"))
+speed2=float(input("enter your second speed in miles per hour"))
+speed3=float(input("enter your third speed in miles per hour"))
+avgspeed=(speed1+speed2+speed3)/3
 highestspeed=
---
+++ mod2day1/day1/proj.py
@@ -0,0 +1,8 @@
+age= int(input("enter your age: "))
+if age<10:
+    print("you are underage.")
+else:
+    if age<20:
+        print("you are a teenager.")
+    else:
+        print("you are older than 20!")
