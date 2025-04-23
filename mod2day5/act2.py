7b198e60fd76f2ebaf106aec84974260589c62e8
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-04-23 09:24:29 +0300

    act2

---
+++ mod2day5/act2.py
@@ -0,0 +1,9 @@
+print("FLOYD'S TRIANGLE:")
+rows=int(input("enter the amount of rows you want"))
+num=1
+
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print(num,end=" ")
+        num=num+1
+    print()
