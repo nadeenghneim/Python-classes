39b2a156abdbd3ab984a77ff8ae5d77edd2d32a8
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-04-28 09:14:09 +0300

    turtleproj

---
+++ mod2day6/proj.py
@@ -0,0 +1,11 @@
+import turtle 
+turtle.Screen().bgcolor("pink")
+turtle.Screen().setup(750,1000)
+ln=100
+sqr=turtle.Turtle()
+
+
+for i in range(4):
+    sqr.forward(ln)
+    sqr.right(90)
+turtle.done()
