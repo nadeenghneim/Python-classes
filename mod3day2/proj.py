722427078ff796c78fa4e675bac1e2dffada5385
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-05-15 16:30:00 +0300

    shutdownproj.py

--- mod3day2/proj.py
+++ mod3day2/proj.py
@@ -1,2 +1,10 @@
-def shut():
-    
+cond1= input("do you want to shut down the system?:")
+cond2=input("do you have admin approval?:")
+def shut(cond1,cond2):
+    if cond1=="yes" and cond2=="yes":
+        return "shutting down"
+    elif cond1=="no" or cond2=="no":
+        return "you cannot shut down at the moment"
+    else:
+        return "sorry, incorrect input."
+print(shut(cond1,cond2))
