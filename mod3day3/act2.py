375e2d98a8a983ada409f388d97d609ae8b83e65
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-05-15 16:55:26 +0300

    act2

---
+++ mod3day3/act2.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    if (i%20==0):
+        print("it is divisible by 20.")
+    elif (i%2==0):
+        print("it is divisible by 2.")  
+    elif(i%5==0):
+        pass
+    else:
+        print(i)
