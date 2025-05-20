eccde811c342e5c0b1d844cc81097ded57469471
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-05-20 16:53:23 +0300

    act1

---
+++ mod3day6/act1.py
@@ -0,0 +1,7 @@
+from datetime import date,time,datetime
+today=date.today()
+now= datetime.now()
+
+print("today's date is", today)
+print("today's current date and time", now)
+print(today.year,today.month,today.day)#with spaces not hyphens
