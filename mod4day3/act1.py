4ec7e1f80c58cd44a19d9e2d47c749be0727e299
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-06-04 10:47:22 +0300

    act1.py

---
+++ mod4day3/act1.py
@@ -0,0 +1,8 @@
+#dictionary operations 
+dict={"Name":"Nadeen","age":17,"hobby":"Basketball","nationality":"Palestinian"}
+dict.pop("hobby")
+print(dict)
+
+print(dict.values())
+dict["age"]=18
+dict.copy
