20d49589625d1ab6fca6e522a30fb5c4ddd7ca4f
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-06-26 16:13:21 +0300

    project

--- mod4day5/proj.py
+++ mod4day5/proj.py
@@ -2,3 +2,6 @@
 my_list=[85,95,99,40,45,51,42,31]
 result= [mark for mark in my_list if mark<50]
 print(result)
+names=["nadeen",'bob','lyn','sara','dan']
+checking=[x for x in names if len(x)==3]
+print("the names containing only three letters are ",checking)
