4d18ba7b903640ca4dea39f8b22c308569e8e27d
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-07-08 15:14:14 +0300

    project

--- mod5day3/proj.py
+++ mod5day3/proj.py
@@ -1,1 +1,16 @@
-#issubclass
+
+class vehicles():
+    def __init__(self):
+        pass
+class bus(vehicles):
+    def __init__(self):
+        pass
+    def pricing(self):
+        max_cap= int(input("enter the maximum capacity of the bus you wish to take:"))
+        base_price=max_cap*100
+        maintenance_price=(max_cap*100)*0.1
+        total_price=base_price+maintenance_price
+        print("the price for riding a bus with this capacity is",total_price)
+        print("this is including a maintenance charge of 10% the original fee which is:",maintenance_price)
+bus1=bus()
+bus1.pricing()
