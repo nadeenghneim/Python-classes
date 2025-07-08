66e1f46aafe43a945f80ffca888aba1958c63fba
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-07-08 15:29:30 +0300

    project

--- mod5day4/proj.py
+++ mod5day4/proj.py
@@ -1,1 +1,11 @@
 #printing a sentence with reverse order using classes 
+class Reverse():
+    def __init__(self,s=''):
+        self.s=s
+        
+    def execute(self):
+        s=input("enter any string:")
+        reversed_s=s[::-1]
+        print("your reversed string is:",reversed_s)
+r=Reverse()
+r.execute()
