1532f04575dfc2723bb6ff00bf3151bd7eea6d9c
nadeenghneim <nadeen.ghneim9@gmail.com>
2025-08-18 10:47:07 +0300

    project

--- mod6day3/proj.py
+++ mod6day3/proj.py
@@ -3,18 +3,22 @@ import random
 pygame.init()
 screen=pygame.display.set_mode((600,600))
 clock=pygame.time.Clock()
-x,y=300,300
+x,y=100,100
+screen.fill("black")
+pygame.display.set_caption("")
+target_x=random.randint(0,600)
+target_y=random.randint(0,600)
 done=False
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done= True 
-    pygame.draw.rect(screen,'indigo',pygame.Rect(200,100,150,75))
+    purple_rect=pygame.draw.rect(screen,'indigo',pygame.Rect(x,y,50,50))
     image=pygame.image.load("smileyface.png")
-    image = pygame.transform.scale(image, (150, 75))
-    sprite_rect=image.get_rect()
-    screen.fill("black")
-    screen.blit(image,sprite_rect)
+    image = pygame.transform.scale(image, (100,75))
+    target= pygame.draw.rect(screen,'black',pygame.Rect(target_x,target_y,40,40))
+
+    screen.blit(image,(300,100))
     pressed= pygame.key.get_pressed()
     if pressed[pygame.K_UP]:
         y-=3
@@ -24,5 +28,16 @@ while not done:
         x-=3
     if pressed[pygame.K_RIGHT]:
         x+=3
+    x=min(max(0,x),600-50)
+    y=min(max(0,y),600-50)
+    font=pygame.font.SysFont('Times New Roman',18)
+    surface=font.render("Hello! Walk over the treasure hidden on the screen to win!!(hint:it won't br visible)",True,'white')
+    text_rect = surface.get_rect()
+    screen.blit(surface,text_rect)
+    if purple_rect.colliderect(target):
+        win_text = font.render("Great Job!!! You win!", True, 'yellow')
+        screen.blit(win_text, (240, 300))
+        
+    pygame.display.flip()
     clock.tick(60)
 pygame.quit()
